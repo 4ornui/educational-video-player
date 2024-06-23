3731d87623d25901857a64a65b583d6f5a996936
4ornui <chornui.7@gmail.com>
2024-06-22 21:19:37 -0400

    Edit Video mode

--- src/actions/addVideoAction.ts
+++ src/actions/addVideoAction.ts
@@ -1,20 +1,39 @@
 import axios from "axios";
 import { ActionProps } from "../interfaces/interfaces";
-import { ICreateComment } from "../interfaces/IComments";
 import { BASE_URL } from "../config";
 import { redirect } from "react-router-dom";
-export const addVideoAction = async ({ request }: ActionProps) => {
+export const addVideoAction = async ({ request, params }: ActionProps) => {
+    const { id } = params;
+    let isCreatMode = !id;
     const formData = await request.formData();
     const videoUrl = formData.get("video_url");
     const title = formData.get("title");
     const userId = formData.get("user_id");
     const description = formData.get("description");
-    await axios.post<ICreateComment>(`${BASE_URL}/api/videos`, {
-        video_url: videoUrl,
-        title: title,
-        user_id: userId,
-        description: description,
-    });
+    let config = {};
+    if (isCreatMode) {
+        config = {
+            method: "POST",
+            url: `${BASE_URL}/api/videos`,
+            data: {
+                video_url: videoUrl,
+                title: title,
+                user_id: userId,
+                description: description,
+            },
+        };
+    } else {
+        config = {
+            method: "PUT",
+            url: `${BASE_URL}/api/videos`,
+            data: {
+                video_id: id,
+                title: title,
+                description: description,
+            },
+        };
+    }
+    await axios(config);
 
     return redirect("/");
 };
--- src/pages/videoDetails.tsx
+++ src/pages/videoDetails.tsx
@@ -1,11 +1,13 @@
+import { useParams } from "react-router-dom";
 import { VideoPlayer } from "../components/VideoPlayer";
 import { CommentsList } from "../components/CommentsList";
 import { Button } from "../components/Button";
 
 export const VideoDetails = () => {
+    let { id } = useParams();
     return (
         <>
-            <Button href="/add-video" text="Edit Video" />
+            <Button href={`/edit-video/${id}`} text="Edit Video" />
             <VideoPlayer />
             <CommentsList />
         </>
